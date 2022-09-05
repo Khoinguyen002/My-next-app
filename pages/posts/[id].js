20bcf00bd4dd4f1b2ccf698ffa52439e6f031630
khoinguyen20022 <khoi.nguyen@kyanon.digital>
2022-09-05 14:37:13 +0700

    update project

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { getPostsId, getPostsById } from "../../lib/post";
 import Layout from "../../components/Layout";
-import { Card, Spinner } from "react-bootstrap";
+import { Card, Carousel, Spinner } from "react-bootstrap";
 import { useRouter } from "next/router";
 import style from "./post.module.scss";
+import fsPromises from "fs/promises";
+import path from "path";
 
-const Post = ({ post }) => {
+const Post = ({ post, objectData }) => {
   const router = useRouter();
-  console.log(router);
+
   if (router.isFallback) {
     return <Spinner animation="grow" role={"banner"}></Spinner>;
   }
@@ -17,8 +19,47 @@ const Post = ({ post }) => {
         <Card.Body>
           <Card.Title className={style["card-title"]}>{post?.title}</Card.Title>
           <Card.Text>{post?.body}</Card.Text>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `<strong>${objectData.name}</strong>`,
+            }}
+          ></div>
         </Card.Body>
       </Card>
+      <Carousel>
+        <Carousel.Item>
+          <img className="d-block w-100" src="/vercel.svg" alt="First slide" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src="/vercel.svg" alt="First slide" />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/1So7VBehCQg"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </Layout>
   );
 };
@@ -31,11 +72,18 @@ export const getStaticPaths = async () => {
   };
 };
 
-export const getStaticProps = async ({ params }) => {
+export const getStaticProps = async (context) => {
+  const { params } = context;
+  console.log(context);
   const post = await getPostsById(params.id);
+  const filePath = path.join(process.cwd(), "/data/book.json");
+  const jsonData = await fsPromises.readFile(filePath);
+  const objectData = JSON.parse(jsonData);
+
   return {
     props: {
       post,
+      objectData,
     },
   };
 };
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { Card } from "react-bootstrap";
+import { Card, Carousel } from "react-bootstrap";
 import Layout from "../../components/Layout";
 import { getPosts } from "../../lib/post";
 import { useRouter } from "next/router";
@@ -22,6 +22,41 @@ const Posts = ({ posts }) => {
           </Card.Body>
         </Card>
       ))}
+      <Carousel>
+        <Carousel.Item>
+          <img className="d-block w-100" src="/vercel.svg" alt="First slide" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Second slide&bg=282c34"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="holder.js/800x400?text=Third slide&bg=20232a"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </Layout>
   );
 };
