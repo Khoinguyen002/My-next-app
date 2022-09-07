db05b636eebadae598cfd31cf7afbbe2f58ea134
khoinguyen20022 <khoi.nguyen@kyanon.digital>
2022-09-07 11:39:21 +0700

    update project

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,19 +5,16 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   type obj = {
-    first: number | undefined | null,
-    2?: number | undefined
-  }
-  const myObj: obj = {first: 3};
-  const myObj2: obj = {first: 3, 2: 3};
-  const getFirst = (price: number, token = null) : void => {
+    first: number | undefined | null;
+    2?: number | undefined;
+  };
+  const myObj: obj = { first: 3 };
+  const myObj2: obj = { first: 3, 2: 3 };
+  const getFirst = (price: number, token = null): void => {
     token = "a";
     console.log(token);
-    
-  }
-  const obbj: [string, string, any] = ["string", "string", 0]
-  
+  };
+  const obbj: [string, string, any] = ["string", "string", 0];
 
-  console.log(obbj[0]);
   res.status(200).json({ name: "John Doe" });
 }
